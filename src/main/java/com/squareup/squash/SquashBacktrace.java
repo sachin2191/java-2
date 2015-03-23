70decb8b5339bcbc9e758a8ce12546cf01feed8b
sachin2191 <sachinkumar2191@gmail.com>
2015-03-23 13:02:44 +0530

    #completed

--- src/main/java/com/squareup/squash/SquashBacktrace.java
+++ src/main/java/com/squareup/squash/SquashBacktrace.java
@@ -7,29 +7,7 @@
 //        http://www.apache.org/licenses/LICENSE-2.0
 //
 //    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-
-package com.squareup.squash;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-/** Creates the Squash stacktrace format for serialization by gson. */
-public final class SquashBacktrace {
-
-  private SquashBacktrace() {
-    // Should not be instantiated: this is a utility class.
-  }
-
-  public static List<SquashException> getBacktraces(Throwable error) {
-    if (error == null) {
+//    distributed under the License is distihi
       return null;
     }
     final List<SquashException> threadList = new ArrayList<SquashException>();
@@ -118,7 +96,7 @@ public final class SquashBacktrace {
       this.message = message;
       this.backtraces = backtraces;
       this.ivars = ivars;
-    }
+    }jjllkokkfinfkn
   }
 
   /** Wrapper object for a stacktrace entry. */
